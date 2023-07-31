51cad50b5d7b626d20d44562b3d4df8980733004
Tommy Monson <tkmonson@protonmail.com>
2023-07-31 19:03:02 -0400

    Update min-knight-moves.py

--- problems/min-knight-moves.py
+++ problems/min-knight-moves.py
@@ -14,13 +14,21 @@ def min_knight_moves(x: int, y: int) -> int:
         if x + y == 0:
             return 0
         if x + y == 2:
-            return 2
+            return 4
 
         memo[(x, y)] = min(dfs(x - 2, y - 1), dfs(x - 1, y - 2)) + 1
         return memo[(x, y)]
 
     return dfs(x, y)
 
+def min_knight_moves2(x1: int, y1: int, x2: int, y2: int) -> int:
+    if x2 < x1:
+        x2 += 2 * (x1 - x2)
+    if y2 < y1:
+        y2 += 2 * (y1 - y2)
+
+    return min_knight_moves(x2 - x1, y2 - y1)
+
 if __name__ == '__main__':
-    print(min_knight_moves(5, 5))
+    print(min_knight_moves(1, 1))
 
